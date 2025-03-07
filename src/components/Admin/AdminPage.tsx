626a679820ef9a18ea023799534ccbd7b2ac1dd0
habtesh-beki <habteshbaki2014@gmail.com>
2025-03-07 01:16:38 -0500

    add admin page

---
+++ src/components/Admin/AdminPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../Header/HeaderAfterLogedIn";
+import Body from "./Admin";
+
+export default function AdminPage() {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+}
